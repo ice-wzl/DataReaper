3f181d84e87c75e32d7627227518dc2b41a3ca96
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2024-02-08 22:25:06 -0500

    Break out files, remove unused imports

---
+++ src/do_keywords.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+from colorama import Fore
+
+
+def key_words(content, ip_addr):
+    interesting_words = [
+        ".aws/",
+        ".bash_history",
+        ".git/",
+        ".python_history",
+        ".ssh/",
+        ".viminfo",
+        ".wg-easy/",
+        ".wget-hsts",
+        "/etc",
+        "/opt",
+        "0day",
+        "brute",
+        "brute_ratel",
+        "bruteforce",
+        "cobalt",
+        "cobalt-strike",
+        "cobalt_strike",
+        "collect",
+        "crypter",
+        "exploit",
+        "gost",
+        "hacking",
+        "havoc",
+        "home/",
+        "id_rsa",
+        "id_ecdsa",
+        "id_dsa",
+        "id_ed25519",
+        "lockbit",
+        "log4j",
+        "malware",
+        "metasploit",
+        "mrlapis",
+        ".msf4",
+        ".msf6",
+        "nessus",
+        "nmap",
+        "notes/",
+        "nuclei",
+        "gorailgun",
+        "passwd",
+        "payload",
+        "pt/",
+        "pp_id_rsa.ppk",
+        "qakbot",
+        "qbot",
+        "ransom",
+        "ransomware",
+        "ratel",
+        "redlinestealer",
+        "revil",
+        "root/",
+        "shadow",
+        "shellcode",
+        "sliver",
+        "sqlmap",
+        "tools/",
+        "victim",
+        "wg/",
+        "wireguard",
+        "wormhole",
+        "key.pem",
+        "crt.pem",
+        "key.key",
+        "crt.key",
+        "cert.crt",
+        ".git-credentials",
+        "password"
+    ]
+
+    word_found = False
+    content = content.decode().lower()
+    interesting_find = ""
+    for i in interesting_words:
+        if i in content:
+            word_found = True
+            print(Fore.GREEN + "\t{} found at {}".format(i, ip_addr.strip()))
+            if (interesting_find == ""):
+                interesting_find = i
+            else:
+                interesting_find = interesting_find + "," + i
+    return word_found, interesting_find
